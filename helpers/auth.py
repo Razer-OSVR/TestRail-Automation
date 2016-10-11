cd1135a8435b6a8db9131d8f9eed8f5c90049bc8
Marcelo Araujo <marcelo.araujo@razerzone.com>
2016-10-11 18:17:05 +0800

    - Remove the hardcoded USER, PWD and URL from source code. - Use ConfigParser module to parse the configuration file. MFM after: 14 days Relnotes: YES

--- helpers/auth.py
+++ helpers/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
+import ConfigParser
 from TestRail.testrail import *
 
 __author__ = "OSVR and contributors."
@@ -8,17 +9,30 @@ __copyright__ = "Copyright 2016, %s" % (__author__)
 __credits__ = ["Marcelo Araujo"]
 __license__ = "Apache 2.0"
 
-#  Globals
-TR_USER = 'YOUR_USER_HERE'
-TR_PWD = 'YOUR_PASSWORD_HERE'
-TR_URL = 'YOUR_TESTRAIL_URL_HERE'
+# Config File.
+config_file_path = "defaults.cfg"
 
+def readConfigFile():
+    TR_URL = TR_USER = TR_PWD = None
+    section = 'TestRail'
+
+    config = ConfigParser.ConfigParser()
+    config.readfp(open(config_file_path))
+
+    TR_USER = config.get(section, 'USER')
+    TR_PWD = config.get(section, 'PWD')
+    TR_URL = config.get(section, 'URL')
+
+    return TR_USER, TR_PWD, TR_URL
 
 def trclient():
     '''
     Set the connection and return it to be
     used by other methods.
     '''
+
+    TR_USER, TR_PWD, TR_USER = readConfigFile()
+
     client = APIClient(TR_URL)
     client.user = TR_USER
     client.password = TR_PWD
